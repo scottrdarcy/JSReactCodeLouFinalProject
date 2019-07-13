4bf6a625ad6dfb07e0631997669aee889b89a782
Scott Darcy <scottrdarcy@yahoo.com>
2019-07-13 14:09:40 -0400

    Changing to fresh react app

--- src/App.js
+++ src/App.js
@@ -1,76 +1,25 @@
-import React, { Component } from "react";
+import logo from "./logo.svg";
+import React, { Root, Component } from "react";
 import { Container, Dropdown, Form, Jumbotron } from "react-bootstrap";
-import Button from "react-bootstrap/Button";
-//import Button from "./component/button";
-//import Menu from "./component/menu";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <Container>
-        <Jumbotron>
-          <h1>Scott's Stock Screener</h1>
+function App() {
+  return (
+    <div className="App">
+      <Dropdown>
+        <Dropdown.Toggle variant="success" id="dropdown-basic">
+          Menu
+        </Dropdown.Toggle>
 
-          <Dropdown>
-            <Dropdown.Toggle variant="success" id="dropdown-basic">
-              Menu
-            </Dropdown.Toggle>
-
-            <Dropdown.Menu>
-              <Dropdown.Item href="App.js">Home</Dropdown.Item>
-              <Dropdown.Item href="journal.js">Journal</Dropdown.Item>
-              <Dropdown.Item href="api.js">API</Dropdown.Item>
-              <Dropdown.Item href="table.js">Tables</Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-        </Jumbotron>
-
-        <Jumbotron>
-          <Form>
-            <Form.Group controlId="exampleForm.ControlInput1">
-              <Form.Label>Stock Symbol</Form.Label>
-              <Form.Control type="email" placeholder="AMZN" />
-            </Form.Group>
-
-            <Button>Buy Now</Button>
-            <Button>30 Days</Button>
-            <Button>60 Days</Button>
-            <Button>90 Days</Button>
-            <Button>Hold</Button>
-            <Button>Sell</Button>
-            <Button>Short</Button>
-
-            <Form.Group controlId="exampleForm.ControlTextarea1">
-              <Form.Label>Explain Reasoning of Selection:</Form.Label>
-              <Form.Control as="textarea" rows="5" />
-            </Form.Group>
-          </Form>
-        </Jumbotron>
-      </Container>
-    );
-  }
+        <Dropdown.Menu>
+          <Dropdown.Item href="home.js">Home</Dropdown.Item>
+          <Dropdown.Item href="journal.js">Journal</Dropdown.Item>
+          <Dropdown.Item href="api.js">API</Dropdown.Item>
+          <Dropdown.Item href="tables.js">Tables</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+    </div>
+  );
 }
 
 export default App;
-
-
-
-/*
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
-
-export default App;  */
