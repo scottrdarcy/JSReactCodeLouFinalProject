a2c3fdcb7601588df0f3699c620b829f646e0264
Scott Darcy <scottrdarcy@yahoo.com>
2019-07-20 16:00:55 -0400

    Added submit button to App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import logo from "./logo.svg";
 import React, { Root, Component } from "react";
-import { Container, Dropdown, Form, Button, Jumbotron } from "react-bootstrap";
+import {
+  Container,
+  Dropdown,
+  Form,
+  Button,
+  Jumbotron,
+  Col,
+  Row
+} from "react-bootstrap";
 import "./App.css";
+
+const buttonhome = {
+  margin: "2px"
+};
+const buttonstyle = {
+  display: 'flex',
+   justifyContent: 'center'
+};
 
 function App() {
   return (
@@ -9,23 +24,29 @@ function App() {
       <Jumbotron>
         <Form>
           <Form.Group controlId="exampleForm.ControlInput1">
-            <Form.Label>Stock Symbol</Form.Label>
+            <Form.Label>Stock Symbol:</Form.Label>
             <Form.Control type="email" placeholder="AMZN" />
           </Form.Group>
 
-          <Button>Buy Now</Button>
-          <Button>30 Days</Button>
-          <Button>60 Days</Button>
-          <Button>90 Days</Button>
-          <Button>Hold</Button>
-          <Button>Sell</Button>
-          <Button>Short</Button>
+          <Button style={buttonhome}>Buy Now</Button>
+          <Button style={buttonhome}>30 Days</Button>
+          <Button style={buttonhome}>60 Days</Button>
+          <Button style={buttonhome}>90 Days</Button>
+          <Button style={buttonhome}>Hold</Button>
+          <Button style={buttonhome}>Sell</Button>
+          <Button style={buttonhome}>Short</Button>
 
           <Form.Group controlId="exampleForm.ControlTextarea1">
             <Form.Label>Explain Reasoning of Selection:</Form.Label>
             <Form.Control as="textarea" rows="5" />
           </Form.Group>
         </Form>
+
+        <Row>
+          <Col>
+            <Button style={buttonstyle}>Sumbit</Button>
+          </Col>
+        </Row>
       </Jumbotron>
     </Container>
   );
